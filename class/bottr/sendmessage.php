fcc07c1792ba0bc43430a382fee49a1373979d71
Raphael Michel <webmaster@raphaelmichel.de>
2010-06-01 16:27:30 +0200

    Help buddies not talking HTML

--- class/bottr/sendmessage.php
+++ class/bottr/sendmessage.php
@@ -14,5 +14,14 @@ if (mb_detect_encoding($body.' ', 'UTF-8, ISO-8859-1') != 'UTF-8') {
 }
 
 if($enc) $body = htmlspecialchars($body);
+
+$plainbody = preg_replace("#<a[^>]*href=['|\"]([^'\">]*)['|\"][^>]*>(.*)</a>#iU", '\\2 (\\1)', $body);
+$plainbody = strip_tags($plainbody);
+
+if($plainbody === $body)
+	$xml = "<message type='chat' to='$to'><body>".$body."</body></message>";
+else
+	$xml = "<message type='chat' to='$to'><html xmlns='http://jabber.org/protocol/xhtml-im'><body xmlns='http://www.w3.org/1999/xhtml'>".$body."</body></html><body>".$plainbody."</body></message>";
+	
 $self->sendXML("<message id='{$self->auth_id}' type='chat' to='$to'><body>".$body."</body></message>", false);
 ?>
